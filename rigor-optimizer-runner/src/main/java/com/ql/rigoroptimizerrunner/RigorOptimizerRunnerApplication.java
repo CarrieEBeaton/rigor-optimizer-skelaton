a3bae9d2fe148f9d5b89d1af7760d7ab03d84bc0
Carrie Beaton <carrie.beaton@outlook.com>
2020-12-03 15:37:13 -0500

    Rigor init

---
+++ rigor-optimizer-runner/src/main/java/com/ql/rigoroptimizerrunner/RigorOptimizerRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.ql.rigoroptimizerrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RigorOptimizerRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RigorOptimizerRunnerApplication.class, args);
+	}
+
+}
